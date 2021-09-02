e7684f06176f0f01be0df4321561d9eeec42c18f
JustDoom <61824552+JustDoom@users.noreply.github.com>
2021-09-02 17:23:29 +1000

    fix _ in users being turned into a space

--- src/main/java/com/justdoom/bettermessages/events/PlayerWorldChange.java
+++ src/main/java/com/justdoom/bettermessages/events/PlayerWorldChange.java
@@ -27,12 +27,17 @@ public class PlayerWorldChange implements Listener {
 
         for (String key : plugin.getConfig().getConfigurationSection("world-change").getKeys(false)) {
             String msg = plugin.handler.doMessage(player, "world-change." + key, plugin);
-            msg = msg.replace("{to}", player.getWorld().getName());
-            msg = msg.replace("{from}", event.getFrom().getName());
+            String to = player.getWorld().getName();
+            String from = event.getFrom().getName();
             if (plugin.getConfig().getBoolean("world-change." + key + ".replace-underscore")) {
-                msg = msg.replace("_", " ");
+                to = to.replace("_", " ");
+                from = from.replace("_", " ");
             }
-            if (plugin.getConfig().getBoolean("world-change." + key + ".enabled") && plugin.getConfig().getString("world-change." + key + ".to").equals(player.getWorld().getName()) && plugin.getConfig().getString("world-change." + key + ".from").equals(event.getFrom().getName())) {
+            msg = msg.replace("{to}", to);
+            msg = msg.replace("{from}", from);
+            if (plugin.getConfig().getBoolean("world-change." + key + ".enabled")
+                    && plugin.getConfig().getString("world-change." + key + ".to").equals(player.getWorld().getName())
+                    && plugin.getConfig().getString("world-change." + key + ".from").equals(event.getFrom().getName())) {
                 for (Player p : Bukkit.getOnlinePlayers()) {
                     plugin.handler.messageType(p, msg, plugin, "world-change." + key);
                 }
