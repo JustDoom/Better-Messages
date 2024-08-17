67ef053b413fbf9949187ce7f63ad54f5c5e7ffd
doom <61824552+JustDoom@users.noreply.github.com>
2024-08-17 20:33:07 +1000

    Fix null error when leaving

--- spigot/src/main/java/com/imjustdoom/bettermessages/util/VanishUtil.java
+++ spigot/src/main/java/com/imjustdoom/bettermessages/util/VanishUtil.java
@@ -6,6 +6,8 @@ import org.bukkit.metadata.MetadataValue;
 public class VanishUtil {
 
     public static boolean isVanished(Player player) {
+        if (player == null) return false;
+
         for (MetadataValue meta : player.getMetadata("vanished")) {
             if (meta.asBoolean()) {
                 return true;
